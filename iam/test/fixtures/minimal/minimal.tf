610c8351ac5df3ebe57fe1dd6eba84cc767134ac
azonirunner <mhickie@gmail.com>
2017-05-25 08:38:12 -0700

    removing rest of IAM folder for not.  Might need to talk with Stephen on this new structure

--- iam/test/fixtures/minimal/minimal.tf
+++
@@ -1,9 +0,0 @@
-// instantiate the cloudtrail module only supplying required parameters with the intent of really exercising the defaults
-
-module "it_minimal" {
-  source = "../../../" //minimal integration test
-
-  app_engineers      = ["appengineer1", "appengineer2"]
-  platform_engineers = ["plateng1", "plateng2"]
-  operators          = ["operator1", "operator2"]
-}
