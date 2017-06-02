afd126af893fa0a75d8be60b758251471770c9f0
azonirunner <mhickie@gmail.com>
2017-06-02 15:15:17 -0700

    removing iam_policy module from this level

--- iam_policy/template_iam_policy.tf
+++
@@ -1,9 +0,0 @@
-data "template_file" "json_policy" {
-  template = "${file("${path.module}/templates/${var.policy_name}.json")}"
-}
-
-resource "aws_iam_policy" "template-cross-account-policy" {
-  name        = "${var.policy_name}"
-  description = "${var.policy_description}"
-  policy      = "${data.template_file.json_policy.rendered}"
-}
--- iam_policy/test/fixtures/minimal/minimal.tf
+++
@@ -1,8 +0,0 @@
-// instantiate the cloudtrail module only supplying required parameters with the intent of really exercising the defaults
-
-module "policy_template" {
-  source = "../../../" //minimal integration test
-
-  policy_name        = "minimal-test"
-  policy_description = "a policy for testing"
-}
